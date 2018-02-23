13f0744cb7a9c05a1d40dfd706e5216743304101
wangshanying <wangshy1983@gmail.com>
2018-02-23 23:51:05 +0900

    03

--- tableViewPro/tableViewPro/MyTableViewController.swift
+++ tableViewPro/tableViewPro/MyTableViewController.swift
@@ -21,23 +21,50 @@ class MyTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 120
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if indexPath.row % 2 == 0 {
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "identifier", for: indexPath)
-            return cell
-        }else{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "identifier2", for: indexPath)
-            return cell
-        }
+//        if indexPath.row % 2 == 0 {
+//
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "identifier", for: indexPath)
+//            return cell
+//        }else{
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "identifier2", for: indexPath)
+//            return cell
+//        }
 
-    
+//    }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "identifier", for: indexPath)
         
+        let title = cell.viewWithTag(100) as! UILabel
+        let detail = cell.viewWithTag(200) as! UILabel
+        
+        if indexPath.row % 2 == 0 {
+            cell.backgroundColor = UIColor.brown
+        }
+        
+        if indexPath.row % 6 == 0 {
+            title.text = "王　山鷹"
+            detail.text = "男"
+        }else if indexPath.row % 6 == 1 {
+            title.text = "ma jiusi"
+            detail.text = "男"
+        }else if indexPath.row % 6 == 2 {
+            title.text = "yan xiaoning"
+            detail.text = "女"
+        }else if indexPath.row % 6 == 3 {
+            title.text = "na renhu"
+            detail.text = "女"
+        }else if indexPath.row % 6 == 4 {
+            title.text = "wang hongzhang"
+            detail.text = "男"
+        }else if indexPath.row % 6 == 5 {
+            title.text = "wu han"
+            detail.text = "男"
+        }
+        return cell
     }
-
 }
 
