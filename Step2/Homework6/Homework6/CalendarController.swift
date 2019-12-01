fa5ad7c92cc7e725f3280544f40c9a4b05365f6b
Margis <majiusi@icloud.com>
2019-12-01 20:28:21 +0900

    homework6 init

---
+++ Step2/Homework6/Homework6/CalendarController.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarController.swift
+//  Homework6
+//
+//  Created by Margis on 2019/12/01.
+//  Copyright © 2019 Margis. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CalendarController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+}
