556a12f9e3a99f702e0559576a08331c54a14164
马九思 <majiusi@icloud.com>
2019-12-15 19:07:35 +0900

    Clear the storyboard , add the navigator controller

--- Step2/Homework7/PhotoStack/PhotoStack/ViewController.swift
+++ Step2/Homework7/PhotoStack/PhotoStack/ViewController.swift
@@ -12,7 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        self.title = "PhotoStack"
+        
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
 
