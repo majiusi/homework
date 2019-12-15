9b120a23fcfef668c4c2e0361b3453382842fa56
马九思 <majiusi@icloud.com>
2019-12-15 18:39:39 +0900

    Homework 7 init

---
+++ Step2/Homework7/PhotoStack/PhotoStack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoStack
+//
+//  Created by 马九思 on 2019/12/15.
+//  Copyright © 2019 马九思. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
